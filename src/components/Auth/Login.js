bc2aa181f0a4479ca543c8880fc5bea116a11a91
Alec Draymore <Draymore26@gmail.com>
2017-08-23 11:38:30 -0400

    Auth routes && basic forms

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    Login
+    <form>
+      <input
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+      />
+    </form>
+  </div>
+);
---
+++ src/components/Auth/Signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    Sign Up
+    <form>
+      <input
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        placeholder="Re-enter Password"
+      />
+    </form>
+  </div>
+);

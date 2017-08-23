d8186df428db8cb8ab1e872bd55e6df57e24a9d3
Alec Draymore <Draymore26@gmail.com>
2017-08-23 11:51:11 -0400

    Fleshed out auth components

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,44 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    Login
-    <form>
-      <input
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-      />
-    </form>
-  </div>
-);
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ target }) {
+    const nextState = {};
+    nextState[target.name] = target.value;
+    this.setState(nextState);
+  }
+
+  render() {
+    return (
+      <div>
+        Login
+        <form>
+          <input
+            type="text"
+            placeholder="Username"
+            value={this.state.username}
+            name="username"
+            onChange={this.onChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            name="password"
+            onChange={this.onChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}

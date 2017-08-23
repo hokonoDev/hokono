87447dd67d48a8a04085fee1200605051c20b9e4
Michael Lam <mmmikeal@gmail.com>
2017-08-23 11:54:25 -0400

    Dropdown filter framework set out for filterbar

--- src/components/Dashboard/FilterBar.js
+++ src/components/Dashboard/FilterBar.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const FilterBar = () => {
-  return (
-    <div>
-    </div>
-  );
+class FilterBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+
 export default FilterBar;
--- src/components/Dashboard/PetList/PetListEntry.js
+++ src/components/Dashboard/PetList/PetListEntry.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 //link to single petpage
-const PetListEntry = ({name, id, imgUrl, link, onClick}) => {
+const PetListEntry = ({name, id, imgUrl}) => {
   return (
     <Link to={`/pet/${id}`}>
-      <div onClick={onClick}>
+      <div>
         <div>{name}</div>
         <img src={imgUrl}/>
       </div>
@@ -17,8 +17,6 @@ PetListEntry.propTypes = {
   name: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   imgUrl: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default PetListEntry;

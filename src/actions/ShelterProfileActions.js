a456034b67988d0a1b708fd7501f9a85e692ef6e
Alec Draymore <Draymore26@gmail.com>
2017-08-24 20:23:04 -0400

    init closes on completion

--- src/actions/ShelterProfileActions.js
+++ src/actions/ShelterProfileActions.js
@@ -1,15 +1,20 @@
 import firebase from '../firebase/index';
 import store from '../store';
+import { signinAction } from './AuthActions';
 
 export const initAction = (payload) => {
   const user = firebase.auth().currentUser;
   console.log(payload);
 
-  firebase.database().ref(`users/${user.uid}`).set(payload);
+  firebase.database().ref(`shelters/${user.uid}`).set(payload);
 
-  user.updateProfile({ displayName: payload.displayName })
+  user.updateProfile({
+    displayName: payload.displayName,
+    email: user.email,
+  })
     .then(() => {
       console.log('successful update', firebase.auth().currentUser);
+      signinAction();
     });
 
   const action = {

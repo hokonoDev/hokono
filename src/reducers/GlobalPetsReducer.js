5e94972e0e6f1587801cffa723a18b493d4b580c
Michael Lam <mmmikeal@gmail.com>
2017-08-28 10:44:51 -0400

    rebasing

--- src/reducers/GlobalPetsReducer.js
+++ src/reducers/GlobalPetsReducer.js
@@ -1,7 +1,11 @@
 export default (state = {}, action) => {
   switch (action.type) {
     case 'GET_ALL_PETS' :
-      return { ...state, ...action.allPets };
+      if (action.payload === "success") {
+
+      } else {
+        return { ...state };
+      }
     default :
       return state;
   }

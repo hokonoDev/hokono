bbea07f8eba44819b3f12ebbecd0f525f35cbb5f
Michael Lam <mmmikeal@gmail.com>
2017-08-31 11:42:21 -0400

    facebook button added

--- src/containers/AuthRouter.js
+++ src/containers/AuthRouter.js
@@ -68,6 +68,15 @@ const AuthRouter = class extends React.Component {
             />
           )}
         />
+        <div id="fb-root"></div>
+          <script>{(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=127121794596439";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'))}
+          </script>
         <Route
           path={`${this.props.match.path}/signup`}
           render={renderProps => (

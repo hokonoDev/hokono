395f33cfd1c57c1f39d65a54cb771423a9b9bf93
Alec Draymore <Draymore26@gmail.com>
2017-09-05 20:07:07 -0400

    distance api helper fn

--- src/components/lib/helpers.js
+++ src/components/lib/helpers.js
@@ -15,4 +15,13 @@ export const getNameFromPetId = (petId) => {
 export const cordsFromAddress = (addr) => {
   return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=${GOOGLE_API_KEY}`)
     .then(res => res.data.results[0].geometry.location);
+};
+
+export const distanceBetweenLocations = (origin, destination) => {
+  if (typeof origin === 'object')
+    origin = `${origin.lat},${origin.lng}`;
+  if (typeof destination === 'object')
+    destination = `${destination.lat},${destination.lng}`;
+  return axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin}&destinations=${destination}&key=${GOOGLE_API_KEY}`)
+    .then(res => res.rows[0].elements[0].distance.text);
 }

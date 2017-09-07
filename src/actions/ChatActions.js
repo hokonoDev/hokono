92451e48c147a9d4707c6c13175c6d1a0a7a0fa8
Michael Lam <mmmikeal@gmail.com>
2017-09-07 11:05:00 -0400

    multiple chat tabs or switch between chats

--- src/actions/ChatActions.js
+++ src/actions/ChatActions.js
@@ -2,33 +2,26 @@ import firebase from '../firebase/index';
 import store from '../store';
 
 export const newChatMsg = (sender, receiver) => {
-  const action = {
-    type:'NEW_CHAT_MSG',
-    sender: sender,
-    receiver: {
-      type: receiver.type,
-      image: receiver.image,
-      text: receiver.text,
-    },
-  }
-
   //update chat database for receiver and sender
   const uid = firebase.auth().currentUser.uid;
   const key = firebase.database().ref(`/accounts/${uid}/chats/${receiver.uid}`).push().key;
   const key2 = firebase.database().ref(`accounts/${receiver.uid}/chats/${uid}`).push().key
   var updates = {};
-  updates[`/accounts/${uid}/chats/${key}`] = sender;
-  updates[`/accounts/${receiver.uid}/chats/${key2}`] = action.receiver;
+  updates[`/accounts/${uid}/chats/${receiver.uid}/${key}`] = sender;
+  updates[`/accounts/${receiver.uid}/chats/${uid}/${key2}`] = receiver;
   firebase.database().ref().update(updates);
 
   //listener (like a socket)
   firebase.database.ref(`/accounts/${uid}/chats/${receiver.uid}`).on('value')
     .then(snapshot => {
       const msgArray = snapshot.val();
-      store.dispatch({
+      console.log("hopefully an array of messages", msgArray);
+      const action = {
         type:'NEW_CHAT_MSG',
         payload: {[receiver.uid]: msgArray},
-      });
+      }
+      console.log("what is the format of this obj ", {[receiver.uid]: msgArray});
+      store.dispatch(action);
     })
 
 }
--- src/reducers/ChatReducer.js
+++ src/reducers/ChatReducer.js
@@ -1,6 +1,7 @@
 export default (state = {}, action) => {
   switch (action.type) {
     case 'NEW_CHAT_MSG' :
+      console.log("reducer correct? ", {state, ...action.payload});
       return {state, ...action.payload};
     default :
       return state;

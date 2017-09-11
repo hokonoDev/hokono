2b3fea4d5b93fd39fefb2cded808a79ef9361231
Michael Lam <mmmikeal@gmail.com>
2017-09-11 11:30:48 -0400

    added home redirect to top of page

--- src/components/LogoBar.js
+++ src/components/LogoBar.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { signoutAction } from '../actions/AuthActions';
+import { Link } from 'react-router-dom';
 
 export default props => (
   <div
     className="row-box-center"
   >
-    <div
-      className="logo-box"
-    >
-      <img
-        className="logo"
-        src="/images/hokono-logo-v1.0.png"
-        alt=""
-      />
-    </div>
+    <Link to="/">
+      <div
+        className="logo-box"
+      >
+        <img
+          className="logo"
+          src="/images/hokono-logo-v1.0.png"
+          alt=""
+        />
+      </div>
+    </Link>
     {
       props.auth.loggedIn ?
         <button
